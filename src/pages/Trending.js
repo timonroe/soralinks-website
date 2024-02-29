a31f05419cd29ee1e83a783be1f34a2705c3bb93
Tim Monroe <timonroe12@gmail.com>
2024-02-29 15:47:37 -0500

    now using news source logos next to headlines, updated application logo

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,5 +1,38 @@
 import { getTrendingHeadlines } from "../news";
 
+const NewsScraperSource = {
+  AP: 'ap',
+  BBC: 'bbc',
+  CNN: 'cnn',
+  EPOCH_TIMES: 'epochtimes',
+  FOX: 'fox',
+  NEWSWEEK: 'newsweek',
+  // WASH_EXAM: 'washexam',
+};
+
+function getLogo(headline) {
+  const { source } = headline;
+  if (source === NewsScraperSource.AP) {
+    return './news-source-logos/ap-logo-200x200.png';
+  }
+  if (source === NewsScraperSource.BBC) {
+    return './news-source-logos/bbc-logo-200x200.png';
+  }
+  if (source === NewsScraperSource.CNN) {
+    return './news-source-logos/cnn-logo-250x250.png';
+  }
+  if (source === NewsScraperSource.EPOCH_TIMES) {
+    return './news-source-logos/epochtimes-logo-200x200.png';
+  }
+  if (source === NewsScraperSource.FOX) {
+    return './news-source-logos/fox-logo-200x200.png';
+  }
+  if (source === NewsScraperSource.NEWSWEEK) {
+    return './news-source-logos/newsweek-logo-200x200.png';
+  }
+  return './news 640 x 414.png';
+}
+
 export default function Trending({ news }) {
   const trendingHeadlines = getTrendingHeadlines(news)
   return (
@@ -9,6 +42,7 @@ export default function Trending({ news }) {
           <ul>
             {trendingHeadlines.map((headline, idx) => (
               <li key={idx}>
+                <img src={getLogo(headline)} alt="News source's logo" />
                 <a href={headline.url} target="_blank" rel="noopener noreferrer">{headline.title}</a>
               </li>
             ))}
